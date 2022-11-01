b352a3a365c921dd0a54fb9120b8459298e866bf
Liam Rogers <LiamRogersUK@gmail.com>
2022-11-01 16:42:42 +0000

    Completed code for Arduino Tutorial 12: Understanding Potentiometers

---
+++ Understanding_Potentiometers/Understanding_Potentiometers.ino
@@ -0,0 +1,17 @@
+int myVoltPin = A2;
+int readVal;
+float V2;
+int wait = 250;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+readVal = analogRead(myVoltPin);
+V2 = (5./1023.)*readVal; 
+Serial.println(V2);
+delay(wait);
+}
