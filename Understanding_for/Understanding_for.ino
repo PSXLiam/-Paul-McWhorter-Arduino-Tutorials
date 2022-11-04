dd54478f293835aafa1599bf0bb815a0a15d2f1a
Liam Rogers <LiamRogersUK@gmail.com>
2022-11-04 15:02:57 +0000

    Modified code to use for loops

--- Understanding_for/Understanding_for.ino
+++ Understanding_for/Understanding_for.ino
@@ -1,6 +1,9 @@
 int yellowPin = 6;
 int redPin = 9;
 int wait = 500;
+int yellowBlink = 3;
+int redBlink = 5;
+int j; //counter
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,37 +13,17 @@ pinMode(redPin, OUTPUT);
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite (yellowPin, HIGH);
-delay(wait);
-digitalWrite (yellowPin, LOW);
-delay(wait);
-digitalWrite (yellowPin, HIGH);
-delay(wait);
-digitalWrite (yellowPin, LOW);
-delay(wait);
-digitalWrite (yellowPin, HIGH);
-delay(wait);
-digitalWrite (yellowPin, LOW);
-delay(wait);
+for(j=1; j<=yellowBlink; j++){
+  digitalWrite (yellowPin, HIGH);
+  delay(wait);
+  digitalWrite (yellowPin, LOW);
+  delay(wait);
+  }
 
-digitalWrite (redPin, HIGH);
-delay(wait);
-digitalWrite (redPin, LOW);
-delay(wait);
-digitalWrite (redPin, HIGH);
-delay(wait);
-digitalWrite (redPin, LOW);
-delay(wait);
-digitalWrite (redPin, HIGH);
-delay(wait);
-digitalWrite (redPin, LOW);
-delay(wait);
-digitalWrite (redPin, HIGH);
-delay(wait);
-digitalWrite (redPin, LOW);
-delay(wait);
-digitalWrite (redPin, HIGH);
-delay(wait);
-digitalWrite (redPin, LOW);
-delay(wait);
+for(j=1; j<=redBlink; j++){
+  digitalWrite (redPin, HIGH);
+  delay(wait);
+  digitalWrite (redPin, LOW);
+  delay(wait);
+  }
 }
