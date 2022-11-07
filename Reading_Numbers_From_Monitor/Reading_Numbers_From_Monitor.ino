f46e4cf8dcfa73dc89e869199005dfa6543845df
Liam Rogers <LiamRogersUK@gmail.com>
2022-11-07 15:32:33 +0000

    Modified code for Arduino Tutorial 18: Blink LED based on user's input in serial monitor

--- Reading_Numbers_From_Monitor/Reading_Numbers_From_Monitor.ino
+++ Reading_Numbers_From_Monitor/Reading_Numbers_From_Monitor.ino
@@ -1,19 +1,27 @@
-int myNumber;
-int wait;
-String msg1="Please Enter Your Number:"; 
-String msg2="Your Number is: "; 
+int numBlinks;
+int j;
+int blinkTime = 500;
+int redPin = 12;
+
+String msg="How Many Blinks do you Want:"; 
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
+pinMode(redPin, OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-Serial.println(msg1);
+Serial.println(msg);
 while (Serial.available()==0){} //While there is no data on the serial port
 
-myNumber = Serial.parseInt();
-Serial.print(msg2);
-Serial.println(myNumber);
+numBlinks = Serial.parseInt();
+for (j=1; j<=numBlinks; j++){
+  digitalWrite(redPin, HIGH);
+  delay(blinkTime);
+  digitalWrite(redPin, LOW);
+  delay(blinkTime);
+  }
 }
