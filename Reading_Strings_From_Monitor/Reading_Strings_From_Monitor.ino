7f3866ef12428edad1092a6c6cb44ee734effdb7
Liam Rogers <LiamRogersUK@gmail.com>
2022-11-07 16:40:20 +0000

    Modified code for Arduino Tutorial 19: Switching on corresponding LEDs based on user string input.

--- Reading_Strings_From_Monitor/Reading_Strings_From_Monitor.ino
+++ Reading_Strings_From_Monitor/Reading_Strings_From_Monitor.ino
@@ -1,20 +1,44 @@
-String myName;
-String msg1 = "What is your Name? ";
-String msg2 = "Hello ";
-String msg3 = ", Welcome to Arduino!";
+int redPin = 12;
+int greenPin = 11;
+int bluePin = 10;
+
+String myColor;
+String msg = "What Color LED? ";
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
+pinMode (redPin, OUTPUT);
+pinMode (greenPin, OUTPUT);
+pinMode (bluePin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-Serial.println(msg1);
+Serial.println(msg);
 while (Serial.available()==0){} //While there is no data on the serial port
+myColor = Serial.readString();
 
-myName = Serial.readString();
-Serial.print(msg2);
-Serial.print(myName);
-Serial.println(msg3);
+if (myColor.equalsIgnoreCase("red")){
+  digitalWrite(redPin, HIGH);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(bluePin, LOW);
+  }
+
+if (myColor.equalsIgnoreCase("green")){
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, HIGH);
+  digitalWrite(bluePin, LOW);
+  }
+
+if (myColor.equalsIgnoreCase("blue")){
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(bluePin, HIGH);
+  }
+if (myColor.equalsIgnoreCase("off")){
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(bluePin, LOW);
+  }
 }
