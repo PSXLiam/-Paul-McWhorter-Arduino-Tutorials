5eba5735abf28b55cb06ca5bab26a6355e57625f
Liam Rogers <LiamRogersUK@gmail.com>
2022-11-08 16:33:58 +0000

    Completed code for Arduino Tutorial 23: Changing Tone of an Active Buzzer

---
+++ Active_Buzzer_Change_Tone/Active_Buzzer_Change_Tone.ino
@@ -0,0 +1,27 @@
+int buzzPin = 8;
+int dt1 = 1;
+int dt2 = 2;
+int j;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(buzzPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for (j=1; j <= 100; j++){
+  digitalWrite(buzzPin, HIGH);
+  delay (dt1);
+  digitalWrite(buzzPin, LOW);
+  delay (dt1);
+  }
+
+for (j=1; j <= 100; j++){
+  digitalWrite(buzzPin, HIGH);
+  delay (dt2);
+  digitalWrite(buzzPin, LOW);
+  delay (dt2);
+  }
+
+}
