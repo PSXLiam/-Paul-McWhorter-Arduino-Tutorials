b94b93e12ed3a1d2599096ca1a3a52e333e71df8
Liam Rogers <LiamRogersUK@gmail.com>
2022-11-08 14:46:36 +0000

    Completed code for Arduino Tutorial 20: Understanding RGB LED's

---
+++ Understanding_RGB_LED/Understanding_RGB_LED.ino
@@ -0,0 +1,49 @@
+int redPin = 8;
+int greenPin = 9;
+int bluePin = 10;
+
+String myColor;
+String msg = "What Colour Do You Want?";
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(redPin, OUTPUT);
+pinMode(greenPin, OUTPUT);
+pinMode(bluePin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.println(msg);
+while (Serial.available()==0){} //While there is no data on the serial port
+myColor = Serial.readString();
+
+if (myColor.equalsIgnoreCase("red")){
+  digitalWrite(redPin, HIGH);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(bluePin, LOW);
+  }
+
+if (myColor.equalsIgnoreCase("green")){
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, HIGH);
+  digitalWrite(bluePin, LOW);
+  }
+
+if (myColor.equalsIgnoreCase("blue")){
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(bluePin, HIGH);
+  }
+if (myColor.equalsIgnoreCase("aqua")){
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(bluePin, LOW);
+  }
+if (myColor.equalsIgnoreCase("off")){
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(bluePin, LOW);
+  }
+}
