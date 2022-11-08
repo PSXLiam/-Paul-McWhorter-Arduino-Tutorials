c561af90e2be20bb5fa48e1fdc5b351018550187
Liam Rogers <LiamRogersUK@gmail.com>
2022-11-08 16:15:15 +0000

    Completed code for Arduino Tutorial 22: Understanding and Using Active Buzzers to Add Sound to Your Project

---
+++ Active_Buzzer/Active_Buzzer.ino
@@ -0,0 +1,22 @@
+int buzzPin = 8;
+int potVal;
+int potPin = A3;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(buzzPin, OUTPUT);
+pinMode(potPin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+potVal = analogRead(potPin);
+Serial.println(potVal);
+while (potVal >= 1000){
+  digitalWrite(buzzPin, HIGH);
+  potVal = analogRead(potPin);
+  Serial.println(potVal);
+  }
+digitalWrite(buzzPin, LOW);
+}
