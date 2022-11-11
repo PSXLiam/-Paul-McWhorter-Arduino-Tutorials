93ade520932f59db594fe42db792e23e22fa3bda
Liam Rogers <LiamRogersUK@gmail.com>
2022-11-11 16:11:09 +0000

    Modified code for Arduino Tutorial 31: Using Servo in a Simple Project Move the position of the servo via a photo resistor

--- Using_Servos/Using_Servos.ino
+++ Using_Servos/Using_Servos.ino
@@ -1,20 +1,27 @@
 #include <Servo.h>
 
 int servoPin = 9;
-int servoPos = 0;
+int servoAngle;
+int lightPin = A4;
+int lightVal;
+int dt = 250;
+
 Servo myServo;
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
 myServo.attach(servoPin);
+pinMode(lightPin, INPUT);
+pinMode(servoPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-myServo.write(servoPos);
-Serial.println("What Angle for the Servo? ");
-while (Serial.available()==0){}
-servoPos = Serial.parseInt();
-myServo.write(servoPos);
+lightVal = analogRead(lightPin);
+//Serial.println(lightVal);
+
+servoAngle = (-18./16.)*lightVal+18.*310./16.;
+//Serial.println(servoAngle);
+myServo.write(servoAngle);
 }
