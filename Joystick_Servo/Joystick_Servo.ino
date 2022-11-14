8d700327ec95785a50bccf062e634da2e74ce806
Liam Rogers <LiamRogersUK@gmail.com>
2022-11-14 16:29:41 +0000

    Modified code for Arduino Tutorial 33: Removed redundant code Add buzzer output when joystick is pressed down

--- Joystick_Servo/Joystick_Servo.ino
+++ Joystick_Servo/Joystick_Servo.ino
@@ -1,12 +1,11 @@
 #include <Servo.h>
 
 int Xpin = A0;
-int Ypin = A1;
 int Spin = 2;
 int XSpin = 9;
+int buzzPin = 7;
 int WVx;
 int Xval;
-int Yval;
 int Sval;
 int dt = 250;
 
@@ -19,25 +18,30 @@ Serial.begin(9600);
 Xservo.attach(XSpin);
 
 pinMode(Xpin, INPUT);
-//pinMode(Ypin, INPUT);
 pinMode(Spin, INPUT);
+digitalWrite(Spin, HIGH);
 pinMode(XSpin, OUTPUT);
+pinMode(buzzPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 Xval = analogRead(Xpin);
 WVx = (180./1023.)*Xval;
-//Yval = analogRead(Ypin);
 Sval = digitalRead(Spin);
 
 Xservo.write(WVx);
 
+if (Sval == 0){
+  digitalWrite(buzzPin, HIGH);
+  }
+else {
+  digitalWrite(buzzPin, LOW );
+  }
+
 delay(dt);
 Serial.print("X Value = ");
 Serial.print(Xval);
-Serial.print(" Y Value = ");
-Serial.print(Yval);
 Serial.print(" Switch State is ");
 Serial.println(Sval);
 
