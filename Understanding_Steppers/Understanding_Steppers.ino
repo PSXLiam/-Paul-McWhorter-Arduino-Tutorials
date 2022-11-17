a95d5256494bbb546d4f9449ac390885c90c6966
Liam Rogers <LiamRogersUK@gmail.com>
2022-11-17 16:11:33 +0000

    Modified code for Arduino Tutorial 35: Have the direction of the stepper motor's movement be toggled by a push button

--- Understanding_Steppers/Understanding_Steppers.ino
+++ Understanding_Steppers/Understanding_Steppers.ino
@@ -3,18 +3,28 @@
 int stepsPerRevolution = 2048;
 int motSpeed = 10;
 int dt = 500;
+
+int buttonPin = 2;
+int motDir = 1;
+int buttonValNew;
+int buttonValOld = 1; //needs initial value
+
 Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
 myStepper.setSpeed(motSpeed);
+pinMode(buttonPin, INPUT);
+digitalWrite(buttonPin, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-myStepper.step(stepsPerRevolution);
-delay(dt);
-myStepper.step(-stepsPerRevolution);
-delay(dt);
+buttonValNew = digitalRead(buttonPin);
+if (buttonValOld == 1 && buttonValNew == 0){
+  motDir = motDir*(-1); 
+  }
+myStepper.step(motDir*1);
+buttonValOld = buttonValNew;
 }
