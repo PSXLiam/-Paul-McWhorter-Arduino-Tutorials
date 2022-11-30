059d312ed52c0a6d1eb73731fa857c16b1171175
Liam Rogers <LiamRogersUK@gmail.com>
2022-11-30 15:58:52 +0000

    Completed code for Arduino Tutorial 43 & 44: Binary Counter with 74HC595 Serial to Parallel Shift Register Understanding Logical Shift Left and Logical Shift Right with the 74HC595

---
+++ Serial_to_Parallel_Shift_Logical_Shift/Serial_to_Parallel_Shift_Logical_Shift.ino
@@ -0,0 +1,25 @@
+int latchPin = 11;
+int clockPin = 9;
+int dataPin = 12;
+int dt = 500;
+
+byte LEDs = 0b00000101;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(latchPin, OUTPUT);
+pinMode(dataPin, OUTPUT);
+pinMode(clockPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(latchPin,LOW);
+shiftOut(dataPin, clockPin, LSBFIRST, LEDs);
+digitalWrite(latchPin,HIGH);
+LEDs=LEDs*2; //Move LEDs Left
+//LEDs=LEDs/2; //Move LEDs Right
+delay(dt);
+
+}
